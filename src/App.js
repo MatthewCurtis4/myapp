3bfdfc9154b5997ebe83eb3b85c87fd339c3a348
RealMCCurtis <mcurtisbusinesses@gmail.com>
2020-08-03 14:16:49 -0400

    moving index file

--- src/App.js
+++ src/App.js
@@ -8,6 +8,9 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <p>
+         yessssssssssir this is how we do itttt!
+        </p>
         <a
           className="App-link"
           href="https://reactjs.org"
