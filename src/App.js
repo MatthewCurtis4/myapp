e331a82c45ff8a3f82de861e572dbd0bdaf21e63
Your Name <you@example.com>
2020-08-04 09:49:22 -0400

    deploy my react website app to github

--- src/App.js
+++ src/App.js
@@ -1,25 +1,12 @@
 import React from 'react';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p>
-         yessssssssssir this is how we do itttt!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <h1>we the best</h1>
+     <h2>Author: Matthew</h2>
     </div>
   );
 }
