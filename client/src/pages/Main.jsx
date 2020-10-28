e45f874f602b8be9da23bde392d5a1ad1eba954f
MatthewCurtis4 <mcurtisbusinesses@gmail.com>
2020-10-28 10:57:01 -0400

    Working on changing apperance

--- client/src/pages/Main.jsx
+++ client/src/pages/Main.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import '../App.css'
+import './App.css'
 const login = process.env.REACT_APP_PORT_BE || 'https://shielded-forest-06718.herokuapp.com/login';
 
 export class Home extends Component {
@@ -7,7 +7,7 @@ export class Home extends Component {
     render() {
         return (
             <div id="grad">
-                <div class='login' style={homeStyle} >
+                <div class="login">
                     <h1>Welcome to SpotiStat</h1>
                     <h2>Login with Spotify to get started</h2>
                     <a className="button" href={login} > LOGIN </a>
@@ -17,11 +17,7 @@ export class Home extends Component {
     }
 }
 
-const homeStyle = { 
-    backgroundColor: '#cc55aa',
-    textAlign:'center',
-    overflow: 'auto'
-}
+
 
 
 export default Home
