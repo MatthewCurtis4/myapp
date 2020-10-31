cffb62f689fcf9780cd7ec9e36f11bcc5729f73a
MatthewCurtis4 <mcurtisbusinesses@gmail.com>
2020-10-31 17:11:09 -0400

    Changed ports so I can test without updating heroku

--- express/app.js
+++ express/app.js
@@ -16,10 +16,11 @@ var cookieParser = require('cookie-parser');
 
 var client_id = process.env.SPOTIFY_CLIENT_ID; // Your client id
 var client_secret = process.env.SPOTIFY_CLIENT_SECRET; // Your secret
-//var client_id = '60accc7e4de3412abc53f336e76c11ec';
-//var client_secret = 'f5e834d6c51d41e9bdfef1ae6cc60211';
-var redirect_uri = process.env.REDIRECT_URI || 'https://shielded-forest-06718.herokuapp.com/callback/';
-var frontend_redirect = process.env.REDIRECT_URI || 'https://quiet-earth-27756.herokuapp.com/InfoPage';
+var redirect_uri = process.env.REDIRECT_URI || 'http://localhost:8888/callback/';
+var frontend_redirect = process.env.REDIRECT_URI || 'http://localhost:3000/InfoPage';
+//var redirect_uri = process.env.REDIRECT_URI || 'https://shielded-forest-06718.herokuapp.com/callback/';
+//var frontend_redirect = process.env.REDIRECT_URI || 'https://quiet-earth-27756.herokuapp.com/InfoPage';
+
 /**
  * Generates a random string containing numbers and letters
  * @param  {number} length The length of the string
