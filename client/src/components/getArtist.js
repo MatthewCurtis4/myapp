211942524ce4325f7151acd1d6f979ac19633386
MatthewCurtis4 <mcurtisbusinesses@gmail.com>
2021-01-21 10:32:21 -0500

    Cleaning up some files (getArtist function here)

--- client/src/components/getArtist.js
+++ client/src/components/getArtist.js
@@ -11,16 +11,12 @@ var result;
     list.push(index+1 + " ");
     list.push(track.name);
     list.push("<br>");
-    //console.log(track.name);
-    //console.log(index+1);
-    string.concat(track.name);
-    //string.concat("<br>");
-    //var brk = list.split(',');
-    var neww = JSON.stringify(list);
-    //var res = brk.join(" <br> ");
 
-   // document.write(track.name);
-   //var result = list.replace(",", "");
+
+    string.concat(track.name);
+
+
+
    var newlist = list.join(",");
    result = newlist.replace(/,/g, '');
     // Removing all the commas
